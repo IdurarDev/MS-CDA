df69a3143b0029ad7543d2d3116a089987410fa6
Hamid Berkaine <hamiddev78@gmail.com>
2022-01-09 18:15:29 +0100

    add attackMonster function and attack function in main.js

--- MS/js/main.js
+++ MS/js/main.js
@@ -7,5 +7,21 @@ class Players {
 }
 
 const playerOne = new Players("You", attack(), pv);
-const playerOne = new Players("Monster", attack(), pv);
+const playerOne = new Players("Monster", attackMonster(), pv);
 
+function attackMonster () {
+  return Math.floor(Math.random() * ((10 - 5) + 1) + 5);
+}
+
+function attack () {
+  my.value -= attackMonster();
+  pvY -= attackMonster();
+  percentY.innerHTML = pvY;
+  let attackMy = Math.floor(Math.random() * ((10 - 3) + 1) + 3);
+  monster.value -= attackMy;
+  pvM -= attackMy;
+  percentM.innerHTML = pvM;
+
+  checkingScore()
+
+}
