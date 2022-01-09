ca363b84fa5261793fec916ef1b5a1d38511efd5
Hamid Berkaine <hamiddev78@gmail.com>
2022-01-09 18:10:18 +0100

    create class players in main.js

---
+++ MS/js/main.js
@@ -0,0 +1,11 @@
+class Players {
+  constructor (name, attack, points) {
+    this.name = name;
+    this.attack = attack;
+    this.points = points;
+  }
+}
+
+const playerOne = new Players("You", attack(), pv)
+const playerOne = new Players("Monster", attack(), pv)
+
